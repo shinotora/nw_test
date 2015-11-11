477ef58553c0025e99107d2553d2f0192b1a0a87
jajhog <majhog2@aol.com>
2015-11-11 12:47:25 +0900

    fix

--- int_range_01.py
+++ int_range_01.py
@@ -1,10 +1,11 @@
 x = 1
 
-for interface in range (x,x+48):
+for interface in range (x,x + 48):
   print "interface gigabitethertnet 0/%d" % interface
   print "switchport mode access"
   print "switchport access vlan 100"
-  print "channel-group %d mode active" % x+1000
+  interface = interface + 1000
+  print "channel-group %d mode active" % interface
   
 
 
