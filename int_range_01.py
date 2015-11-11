09f34e63acbd7930b3173baca79677bad1f28e07
jajhog <majhog2@aol.com>
2015-11-11 12:30:17 +0900

    add channel group

--- int_range_01.py
+++ int_range_01.py
@@ -1,4 +1,11 @@
-for interface in range (1,49):
+x = 1
+
+for interface in range (x,x+48):
   print "interface gigabitethertnet 0/%d" % interface
   print "switchport mode access"
   print "switchport access vlan 100"
+  print "channel-group %d mode active" % x+1000
+  
+
+
+  
