ff316975d782118331fe32bef4bb2c167dd5c32b
shinotora <hamamaruz@yahoo.co.jp>
2016-11-12 00:23:26 +0900

    Update int_range_01.py
    Port 24

--- int_range_01.py
+++ int_range_01.py
@@ -1,6 +1,6 @@
 x = 1
 
-for interface in range (x,x + 48):
+for interface in range (x,x + 24):
   print "interface gigabitethertnet 0/%d" % interface
   print "switchport mode access"
   print "switchport access vlan 100"
